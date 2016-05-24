2ab0510ca6beb12a424955c615c90a38005abd83
Roy Dalal <dalal.roy@gmail.com>
2016-05-24 10:50:27 +0300

    Added general utils class

---
+++ VerifyooBL/src/Logic/Utils/UtilsGeneral.java
@@ -0,0 +1,16 @@
+package Logic.Utils;
+
+public class UtilsGeneral {
+	public String GenerateGestureFeatureMeanKey(String instruction, String paramName)
+	{
+		String key = String.format("%s-%s", instruction, paramName);
+		return key;
+	}
+	
+	public String GenerateStrokeFeatureMeanKey(String instruction, String paramName, int strokeIdx)
+	{
+		String key = String.format("%s-%s-%s", instruction, String.valueOf(strokeIdx) ,paramName);
+		return key;
+	}
+	
+}
