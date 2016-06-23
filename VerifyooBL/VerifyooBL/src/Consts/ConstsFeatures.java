dc41e1d979dc2c740178a27569012d15d596a573
Roy Dalal <dalal.roy@gmail.com>
2016-06-23 09:01:04 +0300

    Removed files

--- VerifyooBL/VerifyooBL/src/Consts/ConstsFeatures.java
+++
@@ -1,7 +0,0 @@
-package Consts;
-
-public class ConstsFeatures {
-	public static final int ACC_CALC_MIN_NUM_EVENTS = 5;
-	public static final int POINT_BY_MIN_NUM_OF_EVENTS = 5;
-	public static final double MIN_VELOCITY_TRESHOLD = 100.0;
-}
--- VerifyooBL/VerifyooBL/src/Consts/ConstsGeneral.java
+++
@@ -1,6 +0,0 @@
-package Consts;
-
-public class ConstsGeneral {
-	public static final int GESTURE_SCORE_CALC_MIN_PARAMS = 20;
-	public static final double GESTURE_SCORE_CALC_MIN_Z_SCORE = 0.7;
-}
