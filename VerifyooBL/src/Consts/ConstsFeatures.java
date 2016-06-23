21694ee8eaec11d5b4c23784c6b2f69d0098fe27
rc59 <rafich1959@gmail.com>
2016-06-23 09:13:33 +0300

    Small Change

--- VerifyooBL/src/Consts/ConstsFeatures.java
+++ VerifyooBL/src/Consts/ConstsFeatures.java
@@ -2,6 +2,6 @@ package Consts;
 
 public class ConstsFeatures {
 	public static final int ACC_CALC_MIN_NUM_EVENTS = 5;
-	public static final int POINT_BY_MIN_NUM_OF_EVENTS = 5;
+	public static final int POINT_BY_MIN_NUM_OF_EVENTS = 6;
 	public static final double MIN_VELOCITY_TRESHOLD = 100.0;
 }
