d04b45f16194611c8c072e3bbba0acfa311a7fb5
Roy Dalal <dalal.roy@gmail.com>
2016-07-03 16:48:36 +0300

    Added hash table to template structure

--- VerifyooBL/src/Data/UserProfile/Extended/TemplateExtended.java
+++ VerifyooBL/src/Data/UserProfile/Extended/TemplateExtended.java
@@ -10,6 +10,8 @@ import Logic.Comparison.Stats.Interfaces.IFeatureMeanData;
 public class TemplateExtended extends Template {	
 	public ArrayList<GestureExtended> ListGestureExtended;
 	
+	protected HashMap<String, ArrayList<GestureExtended>> mHashGesturesByInstruction;
+	
 	private HashMap<String, IFeatureMeanData> mHashFeatureMeans;
 	
 	public TemplateExtended(Template template) {		
@@ -25,11 +27,28 @@ public class TemplateExtended extends Template {
 		Gesture tempGesture;
 		GestureExtended tempGestureExtended;
 		
+		mHashGesturesByInstruction = new HashMap<>();
+		ArrayList<GestureExtended> tempListGestures;
+		
 		for(int idxGesture = 0; idxGesture < template.ListGestures.size(); idxGesture++) {
 			tempGesture = template.ListGestures.get(idxGesture);
 			tempGestureExtended = new GestureExtended(tempGesture, mHashFeatureMeans, idxGesture);
 			
 			ListGestureExtended.add(tempGestureExtended);
+			
+			if(!mHashGesturesByInstruction.containsKey(tempGestureExtended.Instruction)) {
+				tempListGestures = new ArrayList<>();
+				tempListGestures.add(tempGestureExtended);
+				mHashGesturesByInstruction.put(tempGestureExtended.Instruction, tempListGestures);
+			}
+			else {
+				tempListGestures = mHashGesturesByInstruction.get(tempGestureExtended.Instruction);
+				tempListGestures.add(tempGestureExtended);
+			}
 		}
+	}
+	
+	public HashMap<String, ArrayList<GestureExtended>> GetHashGesturesByInstruction() {
+		return mHashGesturesByInstruction;
 	}
 }
--- VerifyooBL/src/Logic/Comparison/TemplateComparer.java
+++ VerifyooBL/src/Logic/Comparison/TemplateComparer.java
@@ -1,26 +1,25 @@
 package Logic.Comparison;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 
 import Data.Comparison.CompareResultSummary;
+import Data.UserProfile.Extended.GestureExtended;
 import Data.UserProfile.Extended.TemplateExtended;
 import Data.UserProfile.Raw.Template;
 import Logic.Comparison.Stats.FeatureMeanData;
 import Logic.Comparison.Stats.StatEngine;
 
-public class TemplateComparer {
-//		
-//	protected Template mTemplateStored;
-//	protected Template mTemplateAuth;
-
+public class TemplateComparer {	
+	
 	protected TemplateExtended mTemplateStored;
 	protected TemplateExtended mTemplateAuth;
 	
 	protected ArrayList<GestureComparer> mListGestureComparers;
 	CompareResultSummary mCompareResultsTemplate;
 	
-	protected boolean mIsSimilarDevices;	
+	protected boolean mIsSimilarDevices;
 	
 	public TemplateComparer()
 	{				
@@ -41,6 +40,59 @@ public class TemplateComparer {
 		mTemplateStored = templateStored;
 		mTemplateAuth = templateAuth;	
 		
+		HashMap<String, Boolean> hashStoredInstructions = new HashMap<>();
+		String tempInstruction;
+		
+		for(int idxGesture = 0; idxGesture < templateStored.ListGestureExtended.size(); idxGesture++) {
+			tempInstruction = templateStored.ListGestureExtended.get(idxGesture).Instruction;
+			if(!hashStoredInstructions.containsKey(tempInstruction)) {
+				hashStoredInstructions.put(tempInstruction, false);
+			}
+		}
+		
+		GestureExtended tempGestureAuth;
+		GestureExtended tempGestureStored;
+		ArrayList<GestureExtended> tempListGestureStored;
+		
+		for(int idxGestureAuth = 0; idxGestureAuth < templateAuth.ListGestureExtended.size(); idxGestureAuth++) {
+			
+			tempGestureAuth = templateAuth.ListGestureExtended.get(idxGestureAuth);
+			tempInstruction = tempGestureAuth.Instruction;
+			
+			if(hashStoredInstructions.containsKey(tempInstruction) && !hashStoredInstructions.get(tempInstruction)) {
+				hashStoredInstructions.remove(tempInstruction);
+				hashStoredInstructions.put(tempInstruction, true);
+				
+				if(templateStored.GetHashGesturesByInstruction().containsKey(tempInstruction)) {
+					tempListGestureStored = templateStored.GetHashGesturesByInstruction().get(tempInstruction);
+					if(tempListGestureStored.size() > 0) {
+						tempGestureStored = tempListGestureStored.get(0);
+						
+						gestureComparer = new GestureComparer(mIsSimilarDevices);
+						
+						gestureComparer.CompareGestures(tempGestureStored, tempGestureAuth); 
+						mListGestureComparers.add(gestureComparer);
+					}
+					else {
+						break;
+					}
+				}
+				else {
+					break;
+				}
+			}
+		}
+		
+		CalculateFinalScore();
+	}
+	
+	public void CompareTemplates1(TemplateExtended templateStored, TemplateExtended templateAuth)
+	{		
+		GestureComparer gestureComparer;			
+				
+		mTemplateStored = templateStored;
+		mTemplateAuth = templateAuth;	
+		
 		if(templateStored.ListGestureExtended.size() == templateAuth.ListGestureExtended.size()) {			
 			for(int idxGesture = 0; idxGesture < templateStored.ListGestureExtended.size(); idxGesture++) {						
 				gestureComparer = new GestureComparer(mIsSimilarDevices);
@@ -55,12 +107,24 @@ public class TemplateComparer {
 	
 	protected void CalculateFinalScore()
 	{
-		double avgScore = 0;
-		for(int idx = 0; idx < mListGestureComparers.size(); idx++) {
-			avgScore += mListGestureComparers.get(idx).GetScore();			
-		}
-		
-		mCompareResultsTemplate.Score = avgScore / mListGestureComparers.size();
+		if(mListGestureComparers.size() == mTemplateAuth.ListGestureExtended.size()) {
+			ArrayList<Double> listScores = new ArrayList<>();
+			
+			double avgScore = 0;
+			for(int idx = 0; idx < mListGestureComparers.size(); idx++) {						
+				listScores.add(mListGestureComparers.get(idx).GetScore());
+			}
+			
+			Collections.sort(listScores);
+			
+			int scoreCount = 0;
+			for(int idxScore = 1; idxScore < listScores.size(); idxScore++) {
+				avgScore += listScores.get(idxScore);
+				scoreCount++;
+			}
+			
+			mCompareResultsTemplate.Score = avgScore / scoreCount;	
+		}		
 	}
 	
 	public double GetScore()
